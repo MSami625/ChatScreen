2468972dcc3a085980d2ed4b17e6b2d0623ea9b2
M Sami <beboolean@gmail.com>
2024-01-31 06:12:52 +0530

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,25 @@
 async function fetchMessages() {
-    try {
-        const response = await fetch("assets/messages.json");
-        const messages = await response.json();
-        return messages;
-    } catch (error) {
-        console.error("Error fetching messages:", error);
-    }
+  try {
+    const response = await fetch("assets/messages.json");
+    const messages = await response.json();
+    return messages;
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+  }
 }
 
 // Function to display messages on the screen
 async function displayMessages() {
-    const messages = await fetchMessages();
-    const chatContainer = document.getElementById("chat-container");
-    let htmlContent = "";
+  const messages = await fetchMessages();
+  const chatContainer = document.getElementById("chat-container");
+  let htmlContent = "";
 
-    messages.forEach((message, index) => {
-        const messageClass =
-            message.sender === "~ You" ? "sent" : "received";
-        htmlContent += `
+  messages.forEach((message, index) => {
+    const messageClass = message.sender === "~ You" ? "sent" : "received";
+    htmlContent += `
             <div class="message-box ${messageClass}">
                 <div class="message-user">
-                <img src="${message.placeholder}" /> 
+                <img src="${message.placeholder}" alt="profileimg" /> 
                 <span>${message.sender}</span>
                 </div>
                 <div class="message-container ">
@@ -29,49 +28,42 @@ async function displayMessages() {
                 </div>
             </div>
             `;
-    });
+  });
 
-    chatContainer.innerHTML = htmlContent;
+  chatContainer.innerHTML = htmlContent;
 }
 
-
-async function fetchContacts(){
-    try{
-        const response=await fetch("assets/contacts.json");
-        const contacts=response.json();
-        return contacts;
-    }catch(error){
-
-console.log("Error fetching Contacts:",error);
-    }
+async function fetchContacts() {
+  try {
+    const response = await fetch("assets/contacts.json");
+    const contacts = response.json();
+    return contacts;
+  } catch (error) {
+    console.log("Error fetching Contacts:", error);
+  }
 }
-
 
 //function to display contacts list
-async function displayContacts(){
-    const contacts=await fetchContacts();
-    const contactsContainer=document.querySelector(".contacts");
+async function displayContacts() {
+  const contacts = await fetchContacts();
+  const contactsContainer = document.querySelector(".contacts");
 
-     let contactsHtml='';
+  let contactsHtml = "";
 
+  contacts.forEach((contact, index) => {
+    let status = "";
+    if (contact.lastonline > 0) {
+      if (contact.lastonline < 60) {
+        status = `Seen ${contact.lastonline} minutes ago`;
+      } else {
+        let hours = Math.floor(contact.lastonline / 60);
+        status = `Seen ${hours} hours ago`;
+      }
+    } else {
+      status = `<span class="green">Online</span>`;
+    }
 
-     contacts.forEach((contact,index)=>{
-
-
-        let status='';
-        if(contact.lastonline>0){
-            if(contact.lastonline<60){
-                status=`Seen ${contact.lastonline} minutes ago`;
-            }else{
-                let hours=Math.floor(contact.lastonline/60);
-                status=`Seen ${hours} hours ago`;
-            }
-        }else {
-            status=`<span class="green">Online</span>`;
-        }
-
-
-        contactsHtml+=`
+    contactsHtml += `
         <div class="contact-box">
                 <img src="${contact.placeholderpic}" alt="profileimg">
                 <div class="contact-details">
@@ -79,35 +71,29 @@ async function displayContacts(){
                      <span>${status}</span>
                 </div>
                </div>
-        `
-     });
+        `;
+  });
 
-
-     contactsContainer.innerHTML=contactsHtml;
-
-
+  contactsContainer.innerHTML = contactsHtml;
 }
-
-
 
 const navToggle = document.querySelector(".hidden");
 const sidebar = document.querySelector(".sidebar");
 
-navToggle.addEventListener('click', function(event){
-    event.stopPropagation(); 
-    sidebar.classList.toggle("show_nav");
+navToggle.addEventListener("click", function (event) {
+  event.stopPropagation();
+  sidebar.classList.toggle("show_nav");
 });
 
 // Close the sidebar when clicking outside of it
-document.body.addEventListener('click', function () {
-    sidebar.classList.remove("show_nav");
+document.body.addEventListener("click", function () {
+  sidebar.classList.remove("show_nav");
 });
 
 // Prevents clicks inside the sidebar from closing it
-sidebar.addEventListener('click', function (event) {
-    event.stopPropagation();
+sidebar.addEventListener("click", function (event) {
+  event.stopPropagation();
 });
-
 
 displayMessages();
 displayContacts();
